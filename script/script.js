9746d4160d30a0ce9a9a5796afe2a0b5a75ac937
蔡忠振 <94763163@qq.com>
2014-05-28 17:58:33 +0800

    git

--- script/script.js
+++ script/script.js
@@ -1,18 +1,13 @@
 'use strict';
 angular.module('App', [])
     .run(function() {
-        console.log('Im app run...');
+        // console.log('Im app run...');
     })
     .controller('ctrl', ['$scope',
         function($scope) {
             $scope.data = '112233aa';
         }
     ])
-    .directive('dire', function() {
-        return {
-            template: '<div>Im a new template!</div>',
-        }
-    })
     .directive('editable', function() {
         return {
             scope: {
@@ -21,26 +16,22 @@ angular.module('App', [])
             restrict: 'A',
             templateUrl: 'template/editable.html',
             controller: function($scope, $element, $attrs) {
+                $scope.directiveTempData = angular.copy($scope.data);
                 $scope.setEdit = function() {
-                    console.log('is setting editable!');
                     $scope.isEdit = true;
-                    $scope.directiveTempData = angular.copy($scope.data);
-                }
-                $scope.setOK = function() {
-                    $scope.isEdit = false;
+                    setTimeout(function() {
+                        $element.children()[0].focus();
+                    }, 10);
                 }
                 $scope.setCancel = function() {
                     $scope.data = angular.copy($scope.directiveTempData);
                     $scope.isEdit = false;
                 }
-                $(".directive-data-input").focusout(function(){
-                    $scope.setOK();
+                $($element.children()[0]).focusout(function() {
+                    $scope.isEdit = false;
                     $scope.$apply();
                 })
-            },
-            link: function(s, e, a) {
-                console.log(s.data);
-                console.log(s);
+
             }
         }
     })
